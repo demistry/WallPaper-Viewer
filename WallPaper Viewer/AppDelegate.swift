29df7e3edc436250104f68b28c4056df29ac06b4
Demistry <dave11ilen@gmail.com>
2019-05-08 00:45:28 +0100

    Initial Commit

---
+++ WallPaper Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WallPaper Viewer
+//
+//  Created by David Ilenwabor on 08/05/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
