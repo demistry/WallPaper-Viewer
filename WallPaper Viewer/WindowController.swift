5e88bf6377936f2622a98be2c047307bfcb41e7a
Demistry <dave11ilen@gmail.com>
2019-05-23 23:34:06 +0100

    Added sharing of images

---
+++ WallPaper Viewer/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  WallPaper Viewer
+//
+//  Created by David Ilenwabor on 23/05/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
